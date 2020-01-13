27c5e0037d6409514a1ceb3a0b2d757eab2a84e7
RetyaMahendra <52005266+RetyaMahendra@users.noreply.github.com>
2020-01-13 11:34:50 +0800

    Add files via upload

---
+++ iperf3_client_fix.py
@@ -0,0 +1,24 @@
+import os
+   
+try:
+    import iperf3
+except ImportError:
+    print ("install iperf3 package .......")
+    os.system("apt install iperf3 -y > /dev/null 2>&1")
+    os.system("apt install python3-pip -y  > /dev/null 2>&1")
+    os.system("pip3 install iperf3 > /dev/null 2>&1")
+finally:
+    import iperf3
+
+client = iperf3.Client()
+client.duration = 10
+client.server_hostname = '10.153.66.56'
+client.port = 5201
+
+print('Connecting to {0}:{1}'.format(client.server_hostname, client.port))
+result = client.run()
+
+if result.error:
+    print(result.error)
+else:
+    print(result.sent_Mbps)
